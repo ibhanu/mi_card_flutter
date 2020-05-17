cda43cad14b5384a1925d059b20ae12f312a9dc6
Bhanu Pratap <pratap.bhanu3434@gmail.com>
2020-05-18 01:28:56 +0530

    Added Gradient

--- lib/main.dart
+++ lib/main.dart
@@ -10,105 +10,123 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              CircleAvatar(
-                radius: 50.0,
-                backgroundImage: AssetImage('assets/images/profile.jpeg'),
-              ),
-              Text(
-                'Bhanu Pratap',
-                style: TextStyle(
-                  fontFamily: 'Pacifico',
-                  fontSize: 40.0,
+        backgroundColor: Colors.indigo[800],
+        body: Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  stops: [
+                0.1,
+                0.3,
+                0.7,
+                1
+              ],
+                  colors: [
+                Colors.green,
+                Colors.blue,
+                Colors.orange,
+                Colors.pink
+              ])),
+          child: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage('assets/images/profile.jpeg'),
+                ),
+                Text(
+                  'Bhanu Pratap',
+                  style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    fontSize: 40.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  'FLUTTER DEVELOPER',
+                  style: TextStyle(
+                    fontFamily: 'SourceSansPro',
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.teal[100],
+                    letterSpacing: 2.5,
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                  width: 200.0,
+                  child: Divider(
+                    color: Colors.black,
+                  ),
+                ),
+                Card(
                   color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Text(
-                'FLUTTER DEVELOPER',
-                style: TextStyle(
-                  fontFamily: 'SourceSansPro',
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.teal[100],
-                  letterSpacing: 2.5,
-                ),
-              ),
-              SizedBox(
-                height: 20.0,
-                width: 200.0,
-                child: Divider(
-                  color: Colors.teal[100],
-                ),
-              ),
-              Card(
-                color: Colors.white,
-                margin: EdgeInsets.symmetric(
-                  vertical: 10.0,
-                  horizontal: 25.0,
-                ),
-                child: ListTile(
-                  leading: Icon(
-                    Icons.phone,
-                    color: Colors.teal,
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10.0,
+                    horizontal: 25.0,
                   ),
-                  title: Text(
-                    '+91 70892 65084',
-                    style: TextStyle(
-                      color: Colors.teal[900],
-                      fontFamily: 'SourceSansPro',
-                      fontSize: 20.0,
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.indigo[800],
+                    ),
+                    title: Text(
+                      '+91 70892 65084',
+                      style: TextStyle(
+                        color: Colors.purple[900],
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 20.0,
+                      ),
                     ),
                   ),
                 ),
-              ),
-              Card(
-                color: Colors.white,
-                margin: EdgeInsets.symmetric(
-                  vertical: 10.0,
-                  horizontal: 25.0,
-                ),
-                child: ListTile(
-                  leading: Icon(
-                    Icons.public,
-                    color: Colors.teal,
+                Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10.0,
+                    horizontal: 25.0,
                   ),
-                  title: Text(
-                    'bhanupratap.me',
-                    style: TextStyle(
-                      color: Colors.teal[900],
-                      fontFamily: 'SourceSansPro',
-                      fontSize: 20.0,
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.public,
+                      color: Colors.indigo[800],
+                    ),
+                    title: Text(
+                      'bhanupratap.me',
+                      style: TextStyle(
+                        color: Colors.purple[900],
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 20.0,
+                      ),
                     ),
                   ),
                 ),
-              ),
-              Card(
-                color: Colors.white,
-                margin: EdgeInsets.symmetric(
-                  vertical: 10.0,
-                  horizontal: 25.0,
-                ),
-                child: ListTile(
-                  leading: Icon(
-                    Icons.email,
-                    color: Colors.teal,
+                Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10.0,
+                    horizontal: 25.0,
                   ),
-                  title: Text(
-                    'contact@bhanupratap.me',
-                    style: TextStyle(
-                      color: Colors.teal[900],
-                      fontFamily: 'SourceSansPro',
-                      fontSize: 20.0,
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.indigo[800],
+                    ),
+                    title: Text(
+                      'contact@bhanupratap.me',
+                      style: TextStyle(
+                        color: Colors.purple[900],
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 20.0,
+                      ),
                     ),
                   ),
-                ),
-              )
-            ],
+                )
+              ],
+            ),
           ),
         ),
       ),
